662daf77497bca75a3947ff6103e94d1f3ae75ed
thiesen <nathan.thiesen@outlook.com>
2015-02-13 11:46:38 -0200

    fix some shit

--- system/grep.zsh
+++ system/grep.zsh
@@ -2,5 +2,7 @@
 # Color grep results
 # Examples: http://rubyurl.com/ZXv
 #
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'
+GREP_OPTIONS='--color=auto'
+GREP_COLOR='1;32'
+
+ alias grep="grep $GREP_OPTIONS"
